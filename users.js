13de7f67592bd6c0db56458cadf04572156f1527
FanatikAsm <140481728+FanatikAsm@users.noreply.github.com>
2023-07-29 20:45:27 +0600

    Json file with information about user data

---
+++ users.js
@@ -0,0 +1,134 @@
+var users={
+    "0":{
+      "id" : 0,
+      "role" : "Admin",
+      "language" : "RU",
+      "subscription" : "Yes",
+      "country" : "Germany",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2017-04-14"
+    },
+    "1":{
+      "id" : 1,
+      "role" : "User",
+      "language" : "RU",
+      "subscription" : "Yes",
+      "country" : "Russia",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2015-11-18"
+    },
+    "2":{
+      "id" : 2,
+      "role" : "Developer",
+      "language" : "DE",
+      "subscription" : "No",
+      "country" : "Germany",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2010-05-11"
+    },
+    "3":{
+      "id" : 3,
+      "role" : "Developer",
+      "language" : "EN",
+      "subscription" : "No",
+      "country" : "Germany",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2000-03-22"
+    },
+    "4":{
+      "id" : 4,
+      "role" : "Developer",
+      "language" : "EN",
+      "subscription" : "Yes",
+      "country" : "Russia",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2004-12-29"
+    },
+    "5":{
+      "id" : 5,
+      "role" : "Developer",
+      "language" : "EN",
+      "subscription" : "No",
+      "country" : "England",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2012-01-01"
+    },
+    "6":{
+      "id" : 6,
+      "role" : "Admin",
+      "language" : "DE",
+      "subscription" : "No",
+      "country" : "Russia",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2017-12-12"
+    },
+    "7":{
+      "id" : 7,
+      "role" : "User",
+      "language" : "RU",
+      "subscription" : "No",
+      "country" : "Germany",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2015-08-03"
+    },
+    "8":{
+      "id" : 8,
+      "role" : "User",
+      "language" : "DE",
+      "subscription" : "No",
+      "country" : "Germany",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2019-02-14"
+    },
+    "9":{
+      "id" : 9,
+      "role" : "User",
+      "language" : "EN",
+      "subscription" : "No",
+      "country" : "England",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2009-11-15"
+    },
+    "10":{
+      "id" : 10,
+      "role" : "User",
+      "language" : "DE",
+      "subscription" : "Yes",
+      "country" : "England",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "No",
+      "from_date" : "2013-03-08"
+    },
+    "11":{
+      "id" : 11,
+      "role" : "User",
+      "language" : "EN",
+      "subscription" : "No",
+      "country" : "England",
+      "region" : "https://i.ru-music.cc/img/song/thumb/58264-artik-asti-я-так-люблю-тебя.jpg",
+      "city" : "Я так люблю тебя",
+      "positivnes" : "Yes",
+      "from_date" : "2015-07-25"
+    }
+};
